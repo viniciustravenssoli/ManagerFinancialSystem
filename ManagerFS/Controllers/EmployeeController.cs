95722137b4eb2d99ba9f85dff3dc95ee0f68d318
vts1978 <vinicioussouza@gmail.com>
2023-05-22 23:34:08 -0300

    Adding Employee Controller

---
+++ ManagerFS/Controllers/EmployeeController.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ManagerFS.Models;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ManagerFS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmployeeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                            select EmployeeId, EmployeeName, Department,
+                            convert(varchar(10), DateOfJoining,120) as DateOfJoining, PhotoFileName               
+                            from
+                            dbo.Employee";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+        [HttpPost]
+        public JsonResult Post(Employee employee)
+        {
+            string query = @"
+                            insert into dbo.Employee
+                            (EmployeeName, Department, DateOfJoining, PhotoFileName
+                     values (@EmployeeName, @Department, @DateOfJoining, @PhotoFileName)
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@DepartmentName", employee.EmployeeName);
+                    myCommand.Parameters.AddWithValue("@Department", employee.Department);
+                    myCommand.Parameters.AddWithValue("@DateOfJoining", employee.DateOfJoining);
+                    myCommand.Parameters.AddWithValue("@PhotoFileName", employee.PhotoFileName);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Sucessfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Employee employee)
+        {
+            string query = @"
+                            update dbo.Employee
+                            set EmployeeName = @EmployeeName,
+                                Department = @Department,
+                                DateOfJoining = @DateOfJoining,
+                                PhotoFileName = @PhotoFileName
+                                where EmployeeId = @EmployeeId
+                                ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@EmployeeId", employee.EmployeeId);
+                    myCommand.Parameters.AddWithValue("@EmployeeName", employee.EmployeeName);
+                    myCommand.Parameters.AddWithValue("@Department", employee.Department);
+                    myCommand.Parameters.AddWithValue("@DateOfJoining", employee.DateOfJoining);
+                    myCommand.Parameters.AddWithValue("@PhotoFileName", employee.PhotoFileName);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Sucessfully");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                            delete from dbo.Employee
+                            where EmployeeId = @EmployeeId
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@EmployeeId", id);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Sucessfully");
+        }
+    }
+}
