14461a3582f249afc475501aff886326b2c876f3
vts1978 <vinicioussouza@gmail.com>
2023-05-23 23:24:44 -0300

    Adding frontend

---
+++ FrontManagerFS/employee.js
@@ -0,0 +1,1 @@
+const employee = {template:`<h1>This is Employee page</h1>`}
---
+++ FrontManagerFS/variables.js
@@ -0,0 +1,4 @@
+const variables = {
+    API_URL:"http://localhost:42071/api/",
+    PHOTO_URL:"http://localhost:42071/photos/"
+}
